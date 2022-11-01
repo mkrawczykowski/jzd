e63c6d20810ad8f26552407eb5ae5891c6bb47fd
Michal Krawczykowski <mkrawczykowski@gmail.com>
2022-11-01 14:38:56 +0100

    gatsby-node.js -> createPages

---
+++ gatsby-node.js
@@ -0,0 +1,23 @@
+exports.createPages = async ({ actions, graphql }) =>{
+  const result = await graphql(`
+    wpgraphql {
+      pages {
+        nodes {
+          id
+        }
+      }
+    }
+  `)
+
+  const pages = wpgraphql.pages.nodes;
+
+  pages.forEach(page => {
+    actions.createPage({
+      path: page.uri,
+      component: require.resolve('./src/templates/page.js'),
+      context: {
+        id: page.id
+      }
+    })
+  })
+}
