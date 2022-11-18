81820653013e4e34bb4b351216462e2b92099218
Michal Krawczykowski <mkrawczykowski@gmail.com>
2022-11-18 15:43:35 +0100

    posts listing in NewestPosts

--- src/components/content/NewestPosts/NewestPosts.js
+++ src/components/content/NewestPosts/NewestPosts.js
@@ -1,12 +1,44 @@
 import React from 'react';
-import { graphql } from "gatsby";
+import { graphql, useStaticQuery } from "gatsby";
 import Container from '../../structure/Container/Container';
 
+
+
 const NewestPosts = ({ data }) => {
+
+  const newestPostsNumber = data.newestPostsNumber;
+  const skip = data.skip;
+  const layout = data.layout;
+
+  const postsQuery = useStaticQuery(graphql`
+  query{
+    wpgraphql {
+      posts {
+        nodes {
+          title
+        }
+      }
+    }
+  }
+`)
+
+const allPosts = postsQuery.wpgraphql.posts.nodes;
+
   return(
     <Container>
       <section>
-        { data.newestPostsNumber }
+        {/* { data.newestPostsNumber }
+        { data.skip }
+        { data.layout } */}
+        { typeof skip }<br></br>
+        { newestPostsNumber + skip }<br></br>
+        sssssss<br></br>
+        <br></br>
+        {
+        allPosts.map((post, index) => {
+          return index + 1 > skip && index + 1 <= newestPostsNumber + skip ? <h2>{ `${index}: ${post.title}` }</h2> : ''
+        }) 
+        }
       </section>
     </Container>
   )
@@ -17,5 +49,7 @@ export default NewestPosts
 export const query = graphql`
   fragment NewestpostsFragment on WPGraphQL_Page_Acfcontentsections_Sections_Newestposts{
     newestPostsNumber
+    skip
+    layout
   }
 `;
