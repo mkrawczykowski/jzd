ee8eb09a476c1a7e2917398f886d664857edb80e
Michal Krawczykowski <mkrawczykowski@gmail.com>
2022-11-19 13:29:40 +0100

    category.js -> wpgraphql posts

--- src/templates/category.js
+++ src/templates/category.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import Layout from '../components/structure/Layout/Layout';
+import Container from '../components/structure/Container/Container';
 
 export const query = graphql`
   query($id: ID!){
     wpgraphql{
       category(id: $id){
         name
+        posts {
+          nodes {
+            id
+            title
+            ACFpostExcerpt {
+              postExcerpt
+            }
+            uri
+            date
+          }
+        }
       }
     }
   }
@@ -14,11 +26,26 @@ export const query = graphql`
 
 const CategoryTemplate = ( { data } ) =>{ 
   const category = data.wpgraphql.category;
+  const posts = data.wpgraphql.category.posts.nodes;
 
   return(
     <Layout>
-      { category.name }
-      {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+      <Container>
+        { category.name }
+        {
+          posts.map(post => {
+            return(
+              <div>
+                <h2>{ post.title }</h2>
+                <p>{ post.ACFpostExcerpt.postExcerpt }</p>
+                <p>{ post.uri }</p>
+                <p>{ post.date }</p>
+              </div> 
+            )
+          })
+        }
+        {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+      </Container>
     </Layout>
   )
 
