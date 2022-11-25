ae8cd156d40473f4e55d4b833eb415d6462dba48
Michał Krawczykowski <mkrawczykowski@gmail.com>
2022-11-25 17:59:29 +0100

    almost ready RoundedButton

--- src/components/partials/RoundedButton/RoundedButton.js
+++ src/components/partials/RoundedButton/RoundedButton.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import * as styles from './RoundedButton.module.scss';
 
 const RoundedButton = ({href, type, label}) => {
   return(
-    <a href={ href } className={ `button button--${type}` }>{ label }</a>
+    <a href={href} className={styles.button}>{label}</a>
+    // <pre>{JSON.stringify(styles, null, 2)}</pre>
   )
 }
 
