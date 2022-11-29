61d08b0e81a02c4a25ed529ea10ac7a2740cf952
Michał Krawczykowski <mkrawczykowski@gmail.com>
2022-11-29 22:50:34 +0100

    changed classes in columns

--- src/components/partials/PostBox/PostBox.js
+++ src/components/partials/PostBox/PostBox.js
@@ -8,11 +8,11 @@ const PostBox = ({id, title, excerpt, uri, date, categories}) => {
   return(
     <div className={styles.postBox}>
       <Row>
-        <Col classes="col-lg-4">
+        <Col classes="col-lg-5">
           {title ? <h2 className={styles.postBox__title}>{title}</h2> : null}
           <PostCategories categories={categories} date={date}></PostCategories>
         </Col>
-        <Col classes="col-lg-4">
+        <Col classes="col-lg-3">
           {excerpt ? <p className={styles.postBox__excerpt}>{excerpt}</p> : null}
           <RoundedButton href={uri}></RoundedButton>         
         </Col>
