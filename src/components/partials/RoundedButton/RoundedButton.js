4231ec9acc0965f4b8f7d0dbc9645abc5dc199b2
Michał Krawczykowski <mkrawczykowski@gmail.com>
2022-12-07 22:40:27 +0100

    better RoundedButton (with flexbox)

--- src/components/partials/RoundedButton/RoundedButton.js
+++ src/components/partials/RoundedButton/RoundedButton.js
@@ -3,8 +3,9 @@ import * as styles from './RoundedButton.module.scss';
 
 const RoundedButton = ({href, type, label}) => {
   return(
-    <a href={href} className={styles.button}>{label}</a>
-    // <pre>{JSON.stringify(styles, null, 2)}</pre>
+    <div className={styles.roundedButton}>
+      <a href={href} className={styles.roundedButton__button}>{label}</a>
+    </div>
   )
 }
 
