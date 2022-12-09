2216c3c505b388af06430aa746f96507a9dfb73c
Michał Krawczykowski <mkrawczykowski@gmail.com>
2022-12-09 23:02:25 +0100

    removed breaks from switch in FlexibleSections

--- src/components/sections/_FlexibleSections/FlexibleSections.js
+++ src/components/sections/_FlexibleSections/FlexibleSections.js
@@ -8,10 +8,8 @@ const FlexibleSections = ({typename, sectionData}) => {
   switch(typename){
     case 'WPGraphQL_Page_Acfcontentsections_Sections_Wysiwygeditor':
       return <WYSIWYGEditor data={sectionData} />
-    break;
     case 'WPGraphQL_Page_Acfcontentsections_Sections_Newestposts':
       return <NewestPosts data={sectionData} />
-    break;
     case 'WPGraphQL_Page_Acfcontentsections_Sections_Newslettersignup':
       return <NewsletterSignup data={sectionData} />
   }
