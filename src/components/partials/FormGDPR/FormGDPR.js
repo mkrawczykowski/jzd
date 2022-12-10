2fb46c6cb3d245aaefecd7088eefaa58b8d7527a
Michał Krawczykowski <mkrawczykowski@gmail.com>
2022-12-10 14:21:07 +0100

    added FormGDPR component

---
+++ src/components/partials/FormGDPR/FormGDPR.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FormGDPR = () => {
+    return(
+        <label for="email">
+            <input type="text" id="email" name="email"></input>
+        </label>
+    )
+}
+
+export default FormGDPR;
