864006df999cb8535c3ec8dafdfe57babfc2a242
Michał Krawczykowski <mkrawczykowski@gmail.com>
2022-12-10 14:22:45 +0100

    displaying content in NewsletterSignup

--- src/components/sections/NewsletterSignup/NewsletterSignup.js
+++ src/components/sections/NewsletterSignup/NewsletterSignup.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import {graphql} from 'gatsby';
 import {Container, Col, Row} from '../../structure/Grid/Grid';
+import FormGDPR from '../../partials/FormGDPR/FormGDPR';
+import * as styles from './newsletterSignup.module.scss';
 
 const NewsletterSignup = ({data}) => {
+    console.log(data);
+    const sectionsHeading = data.sectionsHeading;
+    const text = data.text;
+    const privacyPolicyText = data.privacyPolicyText;
     return(
         <section>
             <Container>
                 <Row>
                     <Col classes="col col-xl-2"></Col>
                     <Col classes="col col-xl-3">
-                        <label for="email">
-                            <input type="text" id="email" name="email"></input>
-                        </label>
+                        {sectionsHeading ? <h2 className={styles.newsletterSignup}>{sectionsHeading}</h2> : null}
+                        {text ? <div dangerouslySetInnerHTML={{__html: text}}></div> : null}
                     </Col>
                     <Col classes="col col-xl-1"></Col>
-                    <Col classes="col col-xl-4"></Col>
+                    <Col classes="col col-xl-4">
+                        <FormGDPR></FormGDPR>
+                    </Col>
                     <Col classes="col col-xl-2"></Col>
                 </Row>
             </Container>
@@ -28,6 +35,6 @@ export const query = graphql`
     fragment NewslettersignupFragment on WPGraphQL_Page_Acfcontentsections_Sections_Newslettersignup{
         privacyPolicyText
         text
-        sectionsheading
+        sectionsHeading
     }
 `
