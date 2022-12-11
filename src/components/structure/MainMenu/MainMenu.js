a03edd8784ebb1dbae6fc79960f6f55b6ce28be0
Michał Krawczykowski <mkrawczykowski@gmail.com>
2022-12-11 11:51:24 +0100

    some coding and styling in MainMenu

--- src/components/structure/MainMenu/MainMenu.js
+++ src/components/structure/MainMenu/MainMenu.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { useStaticQuery, graphql } from 'gatsby';
+import {useStaticQuery, graphql} from 'gatsby';
+import {Container, Row, Col} from '../Grid/Grid';
 import * as styles from './MainMenu.module.scss';
 
 const Mainmenu = () =>{
@@ -19,22 +20,33 @@ const Mainmenu = () =>{
     }
   `)
 
-  const mainmenuItems = mainMenuQuery.wpgraphql.menu.menuItems;
+  const mainmenuItems = mainMenuQuery.wpgraphql.menu.menuItems.nodes;
 
   return(
-    <>
-      <nav className={ styles.mainMenu }>
-        <ul>
-          { mainMenuQuery.wpgraphql.menu.menuItems.nodes.map( link => {
-            return(
-              <li key={link.id}>{ link.label }</li>
-            )
-          })
-          }
-        </ul>
-      </nav>
-      {/* { JSON.stringify(mainMenuQuery, null, 2) } */}
-    </>
+    <Container>
+      <Row>
+        <Col classes="col col-xs-12 col-lg-1"></Col>
+        <Col classes="col col-xs-12 col-lg-10">
+          <nav className={styles.mainNav}>
+            <h1 className={styles.mainNav__logo}>jakzostalemdevem.pl</h1>
+            <ul className={styles.mainMenu}>
+              {mainmenuItems.map(link => {
+                return(
+                  <li key={link.id} className={styles.mainMenu__item}><a href={link.uri} className={styles.mainMenu__link}>{link.label}</a></li>
+                )
+              })
+              }
+            </ul>
+          </nav>
+          <div className={styles.mainNav__hamburger}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </Col>
+        <Col classes="col col-xs-12 col-lg-1"></Col>
+      </Row>
+    </Container>
   )
 }
 
