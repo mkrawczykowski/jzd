383400564f0c064d6d6ce62e1d30a269eb9c803e
Michał Krawczykowski <mkrawczykowski@gmail.com>
2022-12-13 21:01:56 +0100

    MainMenu changing state to active in mobile

--- src/components/structure/MainMenu/MainMenu.js
+++ src/components/structure/MainMenu/MainMenu.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useStaticQuery, graphql} from 'gatsby';
 import {Container, Row, Col} from '../Grid/Grid';
 import * as styles from './MainMenu.module.scss';
 
 const Mainmenu = () =>{
+  const [menuStateClass, setMenuStateClass] = useState('');
+  const changingMenuStateClass = () => {
+    console.log('changing state!');
+    console.log(menuStateClass);
+    menuStateClass.length > 0 ? setMenuStateClass('') : setMenuStateClass('active');
+  }
+
   const mainMenuQuery = useStaticQuery(graphql`
     query{
       wpgraphql {
@@ -29,7 +36,7 @@ const Mainmenu = () =>{
         <Col classes="col col-xs-12 col-lg-10">
           <nav className={styles.mainNav}>
             <h1 className={styles.mainNav__logo}>jakzostalemdevem.pl</h1>
-            <ul className={styles.mainMenu}>
+            <ul className={[styles.mainMenu, styles[menuStateClass]].join(' ')}>
               {mainmenuItems.map(link => {
                 return(
                   <li key={link.id} className={styles.mainMenu__item}><a href={link.uri} className={styles.mainMenu__link}>{link.label}</a></li>
@@ -38,7 +45,7 @@ const Mainmenu = () =>{
               }
             </ul>
           </nav>
-          <div className={styles.mainNav__hamburger}>
+          <div className={styles.mainNav__hamburger} onClick={changingMenuStateClass}>
             <span></span>
             <span></span>
             <span></span>
