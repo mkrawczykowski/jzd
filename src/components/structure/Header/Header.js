af1cf8835400bc75f9462a4bcf363ec2dc2fbcd2
Michał Krawczykowski <mkrawczykowski@gmail.com>
2022-12-14 09:22:31 +0100

    split MainMenu into Header

--- src/components/structure/Header/Header.js
+++ src/components/structure/Header/Header.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Container, Row, Col} from '../Grid/Grid';
 import MainMenu from '../MainMenu/MainMenu';
+import Logo from '../Logo/Logo';
 import * as styles from './Header.module.scss'
 
 const Header = () => {
+    const [headerStateClass, setHeaderStateClass] = useState('');
+    const changingHeaderStateClass = () => {
+        headerStateClass.length > 0 ? setHeaderStateClass('') : setHeaderStateClass('active');
+    }
+
     return(
         <header className={styles.header}>
-            <MainMenu></MainMenu>
+            <Container>
+                <Row>
+                    <Col classes="col col-xs-12 col-lg-1"></Col>
+                    <Col classes="col col-xs-12 col-lg-10">
+                    <nav className={[styles.mainNav, styles[headerStateClass]].join(' ')}>
+                        <Logo></Logo>
+                        <MainMenu></MainMenu>
+                    </nav>
+                    <div className={styles.mainNav__hamburger} onClick={changingHeaderStateClass}>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                    </Col>
+                    <Col classes="col col-xs-12 col-lg-1"></Col>
+                </Row>
+                </Container>
         </header>
     )
 }
--- src/components/structure/MainMenu/MainMenu.js
+++ src/components/structure/MainMenu/MainMenu.js
@@ -1,14 +1,8 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {useStaticQuery, graphql} from 'gatsby';
-import {Container, Row, Col} from '../Grid/Grid';
-import Logo from '../Logo/Logo';
 import * as styles from './MainMenu.module.scss';
 
 const Mainmenu = () =>{
-  const [navStateClass, setNavStateClass] = useState('');
-  const changingNavStateClass = () => {
-    navStateClass.length > 0 ? setNavStateClass('') : setNavStateClass('active');
-  }
 
   const mainMenuQuery = useStaticQuery(graphql`
     query{
@@ -29,30 +23,14 @@ const Mainmenu = () =>{
   const mainmenuItems = mainMenuQuery.wpgraphql.menu.menuItems.nodes;
 
   return(
-    <Container>
-      <Row>
-        <Col classes="col col-xs-12 col-lg-1"></Col>
-        <Col classes="col col-xs-12 col-lg-10">
-          <nav className={[styles.mainNav, styles[navStateClass]].join(' ')}>
-            <Logo></Logo>
-            <ul className={styles.mainMenu}>
-              {mainmenuItems.map(link => {
-                return(
-                  <li key={link.id} className={styles.mainMenu__item}><a href={link.uri} className={styles.mainMenu__link}>{link.label}</a></li>
-                )
-              })
-              }
-            </ul>
-          </nav>
-          <div className={styles.mainNav__hamburger} onClick={changingNavStateClass}>
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
-        </Col>
-        <Col classes="col col-xs-12 col-lg-1"></Col>
-      </Row>
-    </Container>
+    <ul className={styles.mainMenu}>
+      {mainmenuItems.map(link => {
+        return(
+          <li key={link.id} className={styles.mainMenu__item}><a href={link.uri} className={styles.mainMenu__link}>{link.label}</a></li>
+        )
+      })
+      }
+    </ul>
   )
 }
 
