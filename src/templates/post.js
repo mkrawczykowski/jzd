585936308298d19caff0f533df3838493c4eb936
Michał Krawczykowski <mkrawczykowski@gmail.com>
2022-12-29 18:01:12 +0100

    added categories to post.js

--- src/templates/post.js
+++ src/templates/post.js
@@ -2,12 +2,22 @@ import React from 'react';
 import {graphql} from 'gatsby';
 import Layout from '../components/structure/Layout/Layout';
 import FlexibleSections from '../components/sections/_FlexibleSections/FlexibleSections';
+import {Container} from '../components/structure/Grid/Grid';
+import PostCategories from '../components/partials/PostCategories/PostCategories';
 
 export const queryPost = graphql`
   query($id: ID!){
     wpgraphql{
       post(id: $id){
         title
+        date
+        categories {
+          nodes {
+            id
+            name
+            slug
+          }
+        }
         ACFcontentSections{
           sections{
             __typename
@@ -24,9 +34,18 @@ export const queryPost = graphql`
 const PostTemplate = ({data}) =>{ 
   const post = data.wpgraphql.post;
   const flexSections = post.ACFcontentSections.sections;
+  const categories = post.categories.nodes;
+  const date = post.date;
 
   return(
     <Layout>
+      <Container>
+        <h1>{post.title}</h1>
+        {categories}
+        {date}
+        <PostCategories categories={categories} date={date}></PostCategories>
+      </Container>
+      
       {
         flexSections.map((flexSection, index) => {
           const {__typename, ...sectionData} = flexSection;
